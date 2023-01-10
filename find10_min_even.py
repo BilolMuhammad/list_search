4c35460ea0fbdfeca5aee4bed1bcb20574cd227f
PRESTIGE <mamatqulov@gmail.com>
2023-01-10 11:37:57 +0500

    1o

--- find10_min_even.py
+++ find10_min_even.py
@@ -5,5 +5,14 @@ def find_min_even(data):
         data: list of numbers
     returns: minimum even number in the list
     """
-    return 0
 
+    i, min = 0, 0
+    data = data
+    data.sort(reversed=True)
+    while i < len(data):
+        if data[i] % 2 == 0:
+            min = data[i]
+        i += 1
+    if min == 0:
+        return -1
+    return min
