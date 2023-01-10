42fcf4e275368b327de43be0cfc08f6a46409ffa
PRESTIGE <mamatqulov@gmail.com>
2023-01-10 09:55:31 +0500

    o4

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,9 +5,16 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    data = data
-    data.sort()
-    return data.index(data[-1])
+    # i, _m = 0, 0
+    # while i < len(data):
+    #     if data[i] > _m:
+    #         _m = data[i]
+    #     i += 1
+    # return data.index(_m)
+    d = data
+    d.sort()
+    max = d[-1]
+    return data.index(max)
 
 
 print(find_max_index([2, 3, 1, 4, 7, 9]))
